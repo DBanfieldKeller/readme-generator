d670585faf0306db36a8f08dca956a8d58fb3a14
Goat <dpajbk@gmail.com>
2022-10-19 17:41:55 -0400

    question array added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Title of your project?", "Provide a brief description.", "Provide installation instructions.", "Provide usage instructions.", "Provide contribution guidelines.", "Provide test instructions."];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
