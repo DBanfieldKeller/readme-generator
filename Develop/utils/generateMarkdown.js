97efd19624f58951949c7703cca24c6b4ed9ffd8
Goat <dpajbk@gmail.com>
2022-10-20 19:24:38 -0400

    fixed line breaks in readme generator

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,7 +5,7 @@ function renderLicenseBadge(license) {
     const badge = ""
     return badge
   } else {
-    const badge = `<img src ="https://img.shields.io/badge/license-${license}-brightgreen"/>`;
+    const badge = `<img src ="https://img.shields.io/badge/license-${license}-brightgreen"/> </br>`;
     return badge
   }
 }
@@ -15,16 +15,16 @@ function renderLicenseLink(license) {
   let licenseLink
   switch (license) {
     case 'MIT':
-      licenseLink = '<https://opensource.org/licenses/MIT>';
+      licenseLink = 'https://opensource.org/licenses/MIT </br>';
       break;
     case 'APACHE_2.0':
-      licenseLink = '<http://www.apache.org/licenses/LICENSE-2.0>';
+      licenseLink = 'http://www.apache.org/licenses/LICENSE-2.0 </br>';
       break;
     case 'GPL_3.0' :
-      licenseLink = '<http://www.gnu.org/licenses/>';
+      licenseLink = 'http://www.gnu.org/licenses/ </br>';
       break;
     case 'BSD_3' :
-      licenseLink = '<https://opensource.org/licenses/BSD-3-Clause>';
+      licenseLink = 'https://opensource.org/licenses/BSD-3-Clause </br>';
       break;
     case 'None' :
       licenseLink = ''
@@ -34,9 +34,14 @@ function renderLicenseLink(license) {
   return licenseLink
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
+// Create license section
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return 'This application is not covered by any license';
+  } else {
+    return `This application is covered by the ${license} license`
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -61,13 +66,14 @@ function generateMarkdown(data) {
  ## License
  ${renderLicenseBadge(data.license)}
  ${renderLicenseLink(data.license)}
+ ${renderLicenseSection(data.license)}
  
  ## Tests
  ${data.test}
  ## Questions
  If you have any questions, please feel free to reach out via email or at my github below
  
- ${data.email}
+ ${data.email} </br>
  https://github.com/${data.username}
 `;
 }
